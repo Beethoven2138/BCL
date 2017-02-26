fa28165f4e994f78f52989a07f2df5442f33bbf1
Saxon <saxon.supple@gmail.com>
2017-02-26 12:48:30 +0000

    Update main.c

--- src/main.c
+++ src/main.c
@@ -1,3 +1,16 @@
+/*  text_editor is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+    text_editor is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+    You should have received a copy of the GNU General Public License
+    along with text_editor.  If not, see <http://www.gnu.org/licenses/>.*/
+
+//AUTHOR: SAXON SUPPLE
+
 #include <stdio.h>
 #include <stdlib.h>
 

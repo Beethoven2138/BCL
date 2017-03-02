3a494c75efe8941109116d36163a447a9fc3bf9a
Saxon <saxon.supple@gmail.com>
2017-03-02 21:03:10 +0000

    Update fileops.h

--- include/fileops.h
+++ include/fileops.h
@@ -2,9 +2,12 @@
 #define FILEOPS_H
 
 #include <stdio.h>
+#include <buffer.h>
 
-FILE* create_file_buffer(char *file_name);
+char* create_file_buffer(char *file_name);
 
 int copy_file(char *src_file_name, char *dest_file_name);
+
+int buffer_to_file(char *file_name, char *permissions, struct text_buffer *buffer);
 
 #endif

95f51e0a749e2c6ee8b9c66efb04a9aac5dc9645
Saxon <saxon.supple@gmail.com>
2017-03-05 20:43:10 +0000

    Update buffer.c

--- src/buffer.c
+++ src/buffer.c
@@ -11,23 +11,24 @@ int file_to_buffer(char *file_name, struct text_buffer *buffer, struct buffer_no
 		return 0;
 	
 	buffer->head = node;
+	buffer->tail = node;
 
 	int c;
 
 	while(1)
 	{
-		for (int i = 0; i < BUFFER_NODE_LENGTH; i++)
+		for (int i = 0; i < BUFFER_NODE_LENGTH-1; i++)
 		{
 			c = fgetc(fp);
 			if (!feof(fp))
 			{
 				node->text[i] = (char)c;
-				printf("%c", node->text[i]);
 			}
 		
 			else
 				goto exit;//TODO exit, means end of file
 		}
+		//printf("%s\n", node->text);
 		node->next = (buffer_node*)malloc(sizeof(buffer_node));
 		node->next->prev = node;
 		node = node->next;
@@ -38,4 +39,49 @@ int file_to_buffer(char *file_name, struct text_buffer *buffer, struct buffer_no
 exit:
 	fclose(fp);
 	return 1;
+}
+
+int append_char_to_buffer(char c, struct text_buffer *buffer)
+{
+	if (buffer->tail == 0)
+		return 0;
+
+	struct buffer_node *node = buffer->tail;
+
+	if (node->text[BUFFER_NODE_LENGTH-2] != 0)
+	{
+		node->next = (buffer_node*)malloc(sizeof(struct buffer_node));
+		node->next->prev = node;
+		node = node->next;
+		buffer->tail = node;
+	}
+	
+	for (int i = 0; i < BUFFER_NODE_LENGTH-1; i++)
+	{
+		if (node->text[i] == 0)
+		{
+			node->text[i] = c;
+			break;
+		}
+	}
+	
+	return 1;
+}
+
+// TODO: Get rid of the retarded segmentation fault
+void free_buffer(struct text_buffer *buffer)
+{
+        struct buffer_node *tmp;
+	struct buffer_node *head = buffer->head;
+
+	while (head != 0)
+	{
+		tmp = head;
+		
+		head = head->next;
+		free(tmp);
+	}
+
+	buffer->head = 0;
+	buffer->tail = 0;
 }

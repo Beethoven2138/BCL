1235a746c79ab05d4dff8153e26c6c572c17a7ca
Saxon <saxon.supple@gmail.com>
2017-03-12 10:59:18 +0000

    Update buffer.h

--- include/buffer.h
+++ include/buffer.h
@@ -1,14 +1,17 @@
 #ifndef BUFFER_H
 #define BUFFER_H
 
-#define BUFFER_NODE_LENGTH 80
+typedef struct character
+{
+	char value;
+	struct character *next;
+} character;
 
 typedef struct buffer_node
 {
-	unsigned int length;//TODO set the length of the text
-	char text[BUFFER_NODE_LENGTH];
+	int length;
+	struct character *head;
 	struct buffer_node *next;
-	struct buffer_node *prev;
 } buffer_node;
 
 typedef struct text_buffer
@@ -18,10 +21,14 @@ typedef struct text_buffer
 	unsigned int node_count;
 } text_buffer;
 
-int file_to_buffer(char *file_name, struct text_buffer *buffer, struct buffer_node *node);
+void add_char_to_line(struct buffer_node *line, char value, int position);
 
-int append_char_to_buffer(char c, struct text_buffer *buffer);
+int file_to_buffer(char *file_name, struct text_buffer *buffer);
 
 void free_buffer(struct text_buffer *buffer);
+
+char get_letter(struct buffer_node *node, unsigned int position);
+
+void get_line(struct buffer_node *node, char *contents, unsigned int length);
 
 #endif

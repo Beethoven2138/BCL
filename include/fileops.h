c755847bac3757fde197b6e8ddb4d5d5a059bf9f
Saxon <saxon.supple@gmail.com>
2017-03-25 19:44:24 +0000

    Update fileops.h

--- include/fileops.h
+++ include/fileops.h
@@ -1,4 +1,3 @@
-
 #ifndef FILEOPS_H
 #define FILEOPS_H
 
@@ -10,5 +9,7 @@ char* create_file_buffer(char *file_name);
 int copy_file(char *src_file_name, char *dest_file_name);
 
 int buffer_to_file(char *file_name, struct text_buffer *buffer);
+
+void* auto_save(void *file_buffer_name);
 
 #endif

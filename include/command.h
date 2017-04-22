90f255adc25cab10d65d51df188fa8d8e5b7c34b
Saxon <saxon.supple@gmail.com>
2017-04-22 13:39:51 +0100

    Add files via upload

---
+++ include/command.h
@@ -0,0 +1,22 @@
+#ifndef COMMAND_H
+#define COMMAND_H
+
+#include <buffer.h>
+#include <fileops.h>
+#include <tui.h>
+#include <stdlib.h>
+#include <errno.h>
+#include <ncurses.h>
+#include <string.h>
+
+void (*command_state)();
+
+int quit_state(bool modified, struct text_buffer *buffer);
+void default_state();
+void goto_line(struct text_buffer *buffer);
+void save_buffer();
+void change_buffer();
+int shell_state(struct text_buffer *buffer);
+void read_state(struct text_buffer *buffer);
+
+#endif

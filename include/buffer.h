40f72ab1442ccd1ae3e0339dc9fde88a9f6487d7
Saxon <saxon.supple@gmail.com>
2017-04-22 13:40:11 +0100

    Update buffer.h

--- include/buffer.h
+++ include/buffer.h
@@ -27,24 +27,29 @@ typedef struct buffer_node
 {
 	size_t length;
         size_t lineno;//TODO: add this in all the functions
-	size_t xPos;
 	struct character *head;
 	struct buffer_node *next;
+	size_t xPos;
 } buffer_node;
 
 typedef struct text_buffer
 {
+	char *file_name;
+	char *file_buffer_name;
+	bool modified;
 	int x, y;
 	int xPos, yPos;
 	struct buffer_node *head;
 	struct buffer_node *tail;
         size_t node_count;
+	struct buffer_node *current_line;
 
 	struct buffer_node *edit_start;
 	struct buffer_node *edit_end;
 
 	struct window_data *text_win;
 	struct window_data *command_win;
+	//struct window_data *title_win;
 
 	//TODO: Add support for linked list of buffers
 	struct text_buffer *next;
@@ -58,7 +63,7 @@ void add_char_to_line(struct buffer_node *line, char value, int position);
 
 int file_to_buffer(char *file_name, struct text_buffer *buffer);
 
-void free_buffer(struct text_buffer *buffer);
+void free_buffer(struct text_buffer *buffer_to_free);
 
 struct character* get_letter(struct buffer_node *node, size_t position);
 
@@ -70,6 +75,8 @@ void delete_character(struct buffer_node *line, unsigned int position);
 
 void print_buffer(struct text_buffer *buffer);
 
+void print_line(struct text_buffer *buffer, struct buffer_node *line);
+
 void get_prev_line(struct text_buffer buffer, struct buffer_node **line);
 
 void delete_line(struct text_buffer *buffer, struct buffer_node *prev);
@@ -78,7 +85,7 @@ void init_buffer(struct text_buffer *buffer);
 
 void set_edit_buffer(struct text_buffer *buffer, int line);
 
-int longest_line_length(struct text_buffer buffer);
+static int longest_line_length(struct text_buffer buffer);
 
 void update_environment(struct text_buffer *buffer);
 
